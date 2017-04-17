ad8dd10b33d4bb4b614e32351e00fae4e7c9c8d5
Lynk <isaiah.w@outlook.com>
2017-04-17 14:08:51 +0800

    17Apr14:08

---
+++ SIA/SIA.iOS/GetNavData.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+using Xamarin.Forms;
+
+using SIA.iOS;
+
+[assembly: Dependency(typeof(GetNavData))]
+namespace SIA.iOS
+{
+    public class GetNavData : IGetNavData
+    {
+        private string connStr;
+        private string data = "";
+        private List<string> dataList;
+
+        public GetNavData() { }
+
+        public string getQuerySingleRow(string sql)
+        {
+            
+
+            using (SqlConnection connection = new SqlConnection(connStr))
+            {
+
+                SqlCommand command = new SqlCommand(sql, connection);
+                try
+                {
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        data=reader[0].ToString();
+                    }
+                    reader.Close();
+                }
+                catch (Exception ex)
+                {
+                    data = ex.Message;
+                }
+            }
+            return data;
+        }
+
+        List<string> IGetNavData.getQueryMultipleRows(string sql)
+        {
+            using (SqlConnection connection = new SqlConnection(connStr))
+            {
+
+                SqlCommand command = new SqlCommand(sql, connection);
+                try
+                {
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        dataList.Add(reader[0].ToString());
+                    }
+                    reader.Close();
+                }
+                catch (Exception ex)
+                {
+                    dataList.Add(ex.Message);
+                }
+            }
+            return dataList;
+        }
+    }
+}
---
+++ SIA/SIA/IGetNavData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SIA
+{
+    public interface IGetNavData
+    {
+        string getQuerySingleRow(string sql);
+        List<string> getQueryMultipleRows(string sql);
+    }
+}
