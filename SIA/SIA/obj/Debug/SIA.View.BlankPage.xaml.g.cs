46ff67a72ecc4189203a0fc088e4da1e392dea6b
Lynk <isaiah.w@outlook.com>
2017-11-10 14:56:28 +0800

    10Nov2017

--- SIA/SIA/obj/Debug/SIA.View.BlankPage.xaml.g.cs
+++ SIA/SIA/obj/Debug/SIA.View.BlankPage.xaml.g.cs
@@ -11,7 +11,7 @@
 namespace SIA {
     
     
-    [global::Xamarin.Forms.Xaml.XamlFilePathAttribute("D:\\VS Projects\\SIA Original 27Jul2017\\SIA\\SIA\\View\\BlankPage.xaml")]
+    [global::Xamarin.Forms.Xaml.XamlFilePathAttribute("D:\\VS Projects\\SIA\\SIA\\SIA\\View\\BlankPage.xaml")]
     public partial class BlankPage : global::Xamarin.Forms.ContentPage {
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "0.0.0.0")]
